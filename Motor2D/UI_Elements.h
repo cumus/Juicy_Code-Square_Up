54acee39380247d8c1ee016a747db215c917e164
Ruben Sardon <ec4who@hotmail.com>
2020-03-12 03:07:55 +0100

    Added Empty UI_Element class

---
+++ Motor2D/UI_Elements.h
@@ -0,0 +1,20 @@
+#ifndef __UI_ELEMENTS_H__
+#define __UI_ELEMENTS_H__
+
+#include "EventListener.h"
+
+struct RectF
+{
+	float x, y, w, h;
+};
+
+class UI_Element : public EventListener
+{
+public:
+	UI_Element(RectF rect) : rect(rect) {}
+	~UI_Element() {}
+
+	RectF rect;
+};
+
+#endif // __UI_ELEMENTS_H__
