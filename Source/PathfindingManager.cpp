abbe4f248f4e32f7477de58ac440b7ab3f637a7d
Avilgor <35470345+Avilgor@users.noreply.github.com>
2020-03-16 13:11:58 +0100

    Pathfinding Manager
    Added base pathfinding code

---
+++ Source/PathfindingManager.cpp
@@ -0,0 +1,235 @@
+/*#include "p2Defs.h"
+#include "p2Log.h"
+#include "j1App.h"
+#include "PathfindingManager.h"
+#include "brofiler/Brofiler/Brofiler.h"
+
+PathfindingManager::PathfindingManager() : j1Module(), map(NULL), last_path(DEFAULT_PATH_LENGTH), width(0), height(0)
+{
+	name.create("pathfinding");
+}
+
+// Destructor
+PathfindingManager::~j1PathFinding()
+{
+	RELEASE_ARRAY(map);
+}
+
+// Called before quitting
+bool PathfindingManager::CleanUp()
+{
+	LOG("Freeing pathfinding library");
+
+	last_path.Clear();
+	RELEASE_ARRAY(map);
+	return true;
+}
+
+// Sets up the walkability map
+void PathfindingManager::SetMap(uint width, uint height, uchar* data)
+{
+	this->width = width;
+	this->height = height;
+
+	RELEASE_ARRAY(map);
+	map = new uchar[width*height];
+	memcpy(map, data, width*height);
+}
+
+// Utility: return true if pos is inside the map boundaries
+bool PathfindingManager::CheckBoundaries(const iPoint& pos) const
+{
+	return (pos.x >= 0 && pos.x <= (int)width &&
+		pos.y >= 0 && pos.y <= (int)height);
+}
+
+// Utility: returns true is the tile is walkable
+bool PathfindingManager::IsWalkable(const iPoint& pos) const
+{
+	uchar t = GetTileAt(pos);
+	return t != INVALID_WALK_CODE && t > 0;
+}
+
+// Utility: return the walkability value of a tile
+uchar PathfindingManager::GetTileAt(const iPoint& pos) const
+{
+	if (CheckBoundaries(pos))
+		return map[(pos.y*width) + pos.x];
+
+	return INVALID_WALK_CODE;
+}
+
+// To request all tiles involved in the last generated path
+const p2DynArray<iPoint>* j1PathFinding::GetLastPath() const
+{
+	return &last_path;
+}
+
+// PathList ------------------------------------------------------------------------
+// Looks for a node in this list and returns it's list node or NULL
+// ---------------------------------------------------------------------------------
+p2List_item<PathNode>* PathList::Find(const iPoint& point) const
+{
+	p2List_item<PathNode>* item = list.start;
+	while (item)
+	{
+		if (item->data.pos == point)
+			return item;
+		item = item->next;
+	}
+	return NULL;
+}
+
+// PathList ------------------------------------------------------------------------
+// Returns the Pathnode with lowest score in this list or NULL if empty
+// ---------------------------------------------------------------------------------
+p2List_item<PathNode>* PathList::GetNodeLowestScore() const
+{
+	p2List_item<PathNode>* ret = NULL;
+	int min = 65535;
+
+	p2List_item<PathNode>* item = list.end;
+	while (item)
+	{
+		if (item->data.Score() < min)
+		{
+			min = item->data.Score();
+			ret = item;
+		}
+		item = item->prev;
+	}
+	return ret;
+}
+
+// PathNode -------------------------------------------------------------------------
+// Convenient constructors
+// ----------------------------------------------------------------------------------
+PathNode::PathNode() : g(-1), h(-1), pos(-1, -1), parent(NULL)
+{}
+
+PathNode::PathNode(int g, int h, const iPoint& pos, const PathNode* parent) : g(g), h(h), pos(pos), parent(parent)
+{}
+
+PathNode::PathNode(const PathNode& node) : g(node.g), h(node.h), pos(node.pos), parent(node.parent)
+{}
+
+// PathNode -------------------------------------------------------------------------
+// Fills a list (PathList) of all valid adjacent pathnodes
+// ----------------------------------------------------------------------------------
+uint PathNode::FindWalkableAdjacents(PathList& list_to_fill) const
+{
+	iPoint cell;
+	uint before = list_to_fill.list.count();
+
+	// north
+	cell.create(pos.x, pos.y + 1);
+	if (App->pathfinding->IsWalkable(cell))
+		list_to_fill.list.add(PathNode(-1, -1, cell, this));
+
+	// south
+	cell.create(pos.x, pos.y - 1);
+	if (App->pathfinding->IsWalkable(cell))
+		list_to_fill.list.add(PathNode(-1, -1, cell, this));
+
+	// east
+	cell.create(pos.x + 1, pos.y);
+	if (App->pathfinding->IsWalkable(cell))
+		list_to_fill.list.add(PathNode(-1, -1, cell, this));
+
+	// west
+	cell.create(pos.x - 1, pos.y);
+	if (App->pathfinding->IsWalkable(cell))
+		list_to_fill.list.add(PathNode(-1, -1, cell, this));
+
+	return list_to_fill.list.count();
+}
+
+// PathNode -------------------------------------------------------------------------
+// Calculates this tile score
+// ----------------------------------------------------------------------------------
+int PathNode::Score() const
+{
+	return g + h;
+}
+
+// PathNode -------------------------------------------------------------------------
+// Calculate the F for a specific destination tile
+// ----------------------------------------------------------------------------------
+int PathNode::CalculateF(const iPoint& destination)
+{
+	g = parent->g + 1;
+	h = pos.DistanceTo(destination);
+
+	return g + h;
+}
+
+// ----------------------------------------------------------------------------------
+// Actual A* algorithm: return number of steps in the creation of the path or -1 ----
+// ----------------------------------------------------------------------------------
+int PathfindingManager::CreatePath(const iPoint& origin, const iPoint& destination)
+{
+	BROFILER_CATEGORY("CreatePath", Profiler::Color::Azure)
+
+		if ((!IsWalkable(origin)) || (!IsWalkable(destination)))
+		{
+			return -1;
+		}
+	last_path.Clear();
+
+	PathList open, close;
+	PathNode node(0, origin.DistanceNoSqrt(destination), origin, nullptr);
+
+	node.pos = origin;
+	open.list.add(node);
+
+	while ((open.list.count() > 0) && (close.list.count() < MAX_PATH_ITERATIONS)) {
+		p2List_item<PathNode>* item;
+
+		item = open.GetNodeLowestScore();
+		close.list.add(item->data);
+		open.list.del(item);
+
+		if (item->data.pos != destination)
+		{
+			PathList adjacentSquares;
+			close.list.end->data.FindWalkableAdjacents(adjacentSquares);
+
+			for (p2List_item<PathNode>* node_item = adjacentSquares.list.start; node_item != NULL; node_item = node_item->next)
+			{
+				if (close.Find(node_item->data.pos))
+				{
+					continue;
+				}
+				else if (open.Find(node_item->data.pos))
+				{
+					PathNode probable_path = open.Find(node_item->data.pos)->data;
+					node_item->data.CalculateF(destination);
+					if (probable_path.g > node_item->data.g) probable_path.parent = node_item->data.parent;
+				}
+				else
+				{
+					node_item->data.CalculateF(destination);
+					open.list.add(node_item->data);
+				}
+			}
+			adjacentSquares.list.clear();
+		}
+		else
+		{
+			p2List_item<PathNode>* path_item = close.list.end;
+
+			for (; path_item->data.parent != nullptr; path_item = close.Find(path_item->data.parent->pos))
+			{
+
+				last_path.PushBack(path_item->data.pos);
+				if (path_item->data.parent == nullptr) {
+					last_path.PushBack(close.list.start->data.pos);
+				}
+			}
+
+			last_path.Flip();
+			return last_path.Count();
+		}
+	}
+	return -1;
+}*/
---
+++ Source/PathfindingManager.h
@@ -0,0 +1,102 @@
+#ifndef __PATHFINDINGMANAGER_H__
+#define __PATHFINDINGMANAGER_H__
+
+/*
+#include "p2Point.h"
+#include "p2DynArray.h"
+#include "p2List.h"
+*/
+
+/*#define DEFAULT_PATH_LENGTH 50
+#define INVALID_WALK_CODE 255
+#define NORMAL_MOVEMENT_COST 1
+#define DIAGONAL_MOVEMENT_COST 2
+#define MAX_PATH_ITERATIONS 150
+
+class PathfindingManager
+{
+public:
+
+	PathfindingManager();
+
+	// Destructor
+	~PathfindingManager();
+
+	// Called before quitting
+	bool CleanUp();
+
+	// Sets up the walkability map
+	void SetMap(uint width, uint height, uchar* data);
+
+	// Main function to request a path from A to B
+	int CreatePath(const iPoint& origin, const iPoint& destination);
+
+	// To request all tiles involved in the last generated path
+	const p2DynArray<iPoint>* GetLastPath() const;
+
+	// Utility: return true if pos is inside the map boundaries
+	bool CheckBoundaries(const iPoint& pos) const;
+
+	// Utility: returns true is the tile is walkable
+	bool IsWalkable(const iPoint& pos) const;
+
+	// Utility: return the walkability value of a tile
+	uchar GetTileAt(const iPoint& pos) const;
+
+private:
+
+	// size of the map
+	uint width = 0u;
+	uint height = 0u;
+	// all map walkability values [0..255]
+	uchar* map = nullptr;
+	// we store the created path here
+	p2DynArray<iPoint> last_path;
+};
+
+// forward declaration
+struct PathList;
+
+// ---------------------------------------------------------------------
+// Pathnode: Helper struct to represent a node in the path creation
+// ---------------------------------------------------------------------
+struct PathNode
+{
+	// Convenient constructors
+	PathNode();
+	PathNode(int g, int h, const iPoint& pos, const PathNode* parent);
+	PathNode(const PathNode& node);
+
+	// Fills a list (PathList) of all valid adjacent pathnodes
+	uint FindWalkableAdjacents(PathList& list_to_fill) const;
+	// Calculates this tile score
+	int Score() const;
+	// Calculate the F for a specific destination tile
+	int CalculateF(const iPoint& destination);
+
+	// -----------
+	int g = 0;
+	int h = 0;
+	iPoint pos = iPoint({ 0, 0 });
+	const PathNode* parent = nullptr; // needed to reconstruct the path in the end
+};
+
+// ---------------------------------------------------------------------
+// Helper struct to include a list of path nodes
+// ---------------------------------------------------------------------
+struct PathList
+{
+	// Looks for a node in this list and returns it's list node or NULL
+	p2List_item<PathNode>* Find(const iPoint& point) const;
+
+	// Returns the Pathnode with lowest score in this list or NULL if empty
+	p2List_item<PathNode>* GetNodeLowestScore() const;
+
+	// -----------
+	// The list itself, note they are not pointers!
+	p2List<PathNode> list;
+};
+*/
+
+
+#endif 
