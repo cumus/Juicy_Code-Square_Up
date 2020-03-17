248676b6fbe565f71985c64a134911bfd059e165
Ruben Sardon <ec4who@hotmail.com>
2020-03-17 17:26:09 +0100

    Fix Button undeclared functions

--- Source/UI_Button.cpp
+++ Source/UI_Button.cpp
@@ -2,6 +2,13 @@
 #include "Application.h"
 #include "Render.h"
 
+UI_Button::UI_Button(EditorWindow* window, RectF rect)
+	: UI_Element(window, BUTTON, rect)
+{}
+
+UI_Button::~UI_Button()
+{}
+
 bool UI_Button::Draw() const
 {
 	return App->render->DrawQuadNormCoords(GetTargetNormRect(), { 0, 0, 0, 255 });
