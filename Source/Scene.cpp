bc10208d92be25125aaea50a64e8a28b4492d5ae
Ruben Sardon <ec4who@hotmail.com>
2020-06-15 23:25:54 +0200

    remove stress test

--- Source/Scene.cpp
+++ Source/Scene.cpp
@@ -440,7 +440,6 @@ void Scene::RecieveEvent(const Event& e)
 void Scene::LoadMainScene()
 {
 	OPTICK_EVENT();
-	App->StressTest();
 	map.Load("maps/iso.tmx");
 	App->audio->PlayMusic("audio/Music/alexander-nakarada-buzzkiller.ogg");
 	App->fogWar.Init();
